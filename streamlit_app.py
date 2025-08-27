ae8df40fac59d9f39cf47145d07fb9bcf0c21b25
Fryzzicola <fryzzicol4@libero.it>
2025-08-27 11:45:21 +0200

    v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ page_bg_img = f"""
     background-image: url("data:image/jpeg;base64, {img}");
     background-size: 5%;        /* Rimpicciolita al 50% */
     background-repeat: no-repeat;/* Non ripetuta */
-    background-position: left-up; /* Centrata */
+    background-position: right bottom; /* Centrata */
 }}
 [data-testid="stHeader"] {{
     background-color: rgba(0, 0, 0, 0);
